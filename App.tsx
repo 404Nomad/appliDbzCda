a34f5811f4eca7afc0b5eec96a61a5b647ae619d
404Nomad <richtofen60@gmail.com>
2025-05-12 15:59:03 +0200

    Refactor App.tsx to use our navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import MainNavigation from './src/navigations/MainNavigation';
 
 const App = () => {
   console.log('App component rendered');
   
   return (
-    <View>
-      <Text style={{color: 'black'}}>App</Text>
-    </View>
+    <NavigationContainer>
+      <MainNavigation />
+    </NavigationContainer>
   )
 }
 
