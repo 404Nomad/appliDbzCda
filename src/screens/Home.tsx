5eac271be25014c93d3e75c4b1bdad5cbc43e9a2
404Nomad <richtofen60@gmail.com>
2025-05-13 16:32:14 +0200

    Implement character fetching in Home component using Redux and add useSelector

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,23 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppDispatch } from '../store/store'
+import { fetchCharacters } from '../store/characters/characterSlice'
+import { RootState } from '../store/store'
 
 const Home = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(fetchCharacters(1, 10)); // Fetch characters when the component mounts, page 1 et limit 10
+  }, [dispatch]);
+
+  const { characters, loading, error } = useSelector(
+    (state: RootState) => state.Characters,
+  );
+
+  console.log('characters', characters);
+
   return (
     <View>
       <Text>Home</Text>
