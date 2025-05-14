63740d2f0bead654c74c3f512942a5f7914adc06
404Nomad <richtofen60@gmail.com>
2025-05-14 11:59:57 +0200

    Add onPress handler to CharacterCard and integrate selection logic in Home component + type HomeScreenNavigationProp to type the navigation

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,17 +2,21 @@ import { View, Text, StyleSheet, FlatList } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { AppDispatch } from '../store/store'
-import { fetchCharacters } from '../store/characters/characterSlice'
+import { fetchCharacters, setSelectedCharacterId } from '../store/characters/characterSlice'
 import { RootState } from '../store/store'
 import { useNavigation } from '@react-navigation/native'
 import Loading from '../components/ui/Loading'
 import HomeHeader from '../components/section/HomeHeader'
 import CharacterCard from '../components/ui/CharacterCard'
+import { StackNavigationProp } from '@react-navigation/stack'
+import { RootStackParamList } from '../navigations/MainNavigation'
+
+type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
 
 const Home = () => {
   // on recupere les hooks
   const dispatch = useDispatch<AppDispatch>(); // on recupere le hook dispatch
-  const navigate = useNavigation(); // on recupere le hook de navigation
+  const navigation = useNavigation<HomeScreenNavigationProp>(); // on recupere le hook de navigation
 
   // on declare nos states
   const [currentPage, setCurrentPage] = useState(1); // State to manage the current page, 1 by default
@@ -32,6 +36,12 @@ const Home = () => {
   );
 
   console.log('characters', characters);
+
+  // handleselectCharacter
+  const handleSelectCharacter = (characterId: number) => {
+    dispatch(setSelectedCharacterId(characterId)); // on dispatch l'action pour set le personnage selectionne
+    navigation.navigate('Detail', {characterId}); // on navigue vers la page de details du personnage
+  }
 
   // on traite le cas du chargement des données
   // si loading est vrai et qu'il n'y a pas de personnages alors on affiche le loading
@@ -54,11 +64,11 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <HomeHeader />
-      <FlatList  
+      <FlatList
         data={characters?.items || []} // on affiche les personnages, tableau vide si pas de personnages
         keyExtractor={(item) => item.id.toString()} // on utilise l'id du personnage comme clé
-        renderItem={( {item} ) => (
-          <CharacterCard character={item}/>
+        renderItem={({ item }) => (
+          <CharacterCard character={item} onPress={() => handleSelectCharacter(item.id)} />
         )}
       />
     </View>
