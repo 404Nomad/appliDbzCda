76dac989aec16dd88df435d3369cb7e2c92563b1
404Nomad <richtofen60@gmail.com>
2025-05-14 09:54:16 +0200

    Refactor Home component to streamline character loading logic and improve readability

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,30 +7,35 @@ import { RootState } from '../store/store'
 import { useNavigation } from '@react-navigation/native'
 
 const Home = () => {
+  // on recupere les hooks
   const dispatch = useDispatch<AppDispatch>(); // on recupere le hook dispatch
   const navigate = useNavigation(); // on recupere le hook de navigation
 
+  // on declare nos states
   const [currentPage, setCurrentPage] = useState(1); // State to manage the current page, 1 by default
+  const [refreshing, setRefreshing] = useState(false); // State to manage the refreshing state
 
   useEffect(() => {
-    // This effect will run when the component mounts
-  
-    return () => {
-      // This cleanup function will run when the component unmounts
-      // You can perform any necessary cleanup here
-    }
-  }, []); // Empty dependency array means this effect runs once when the component mounts
-  
-
-  useEffect(() => {
-    dispatch(fetchCharacters(currentPage, 10)); // Fetch characters when the component mounts, page 1 et limit 10
+    loadCharacters(); // Load characters when the component mounts
   }, [dispatch, currentPage]); // si currentpage change, on refait la requete
+
+  //
+  const loadCharacters = () => {
+    dispatch(fetchCharacters(currentPage, 10)); // Fetch characters when the component mounts, page 1 et limit 10
+  };
 
   const { characters, loading, error } = useSelector(
     (state: RootState) => state.Characters,
   );
 
   console.log('characters', characters);
+
+  // cas du chargement des données
+  if (!loading) {
+    return <Text style={{ color: 'black'}}>'Chargement...'</Text>; // Affiche un message de chargement
+  }
+
+
 
   return (
     <View>
