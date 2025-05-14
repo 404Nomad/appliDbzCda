15eb2377b0d44d963a21c35d5ec39c4cbf268b0f
404Nomad <richtofen60@gmail.com>
2025-05-14 12:10:05 +0200

    Refactor CharacterCard component to include TypeScript types and implement onPress functionality for character selection, add images and style

--- src/components/ui/CharacterCard.tsx
+++ src/components/ui/CharacterCard.tsx
@@ -1,12 +1,51 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native'
 import React from 'react'
+import { Character } from '../../types/character'
 
-const CharacterCard = ({character, onPress}) => {
+interface CharacterCardProps {
+  character: Character,
+  onPress: (characterId: number) => void
+}
+
+const CharacterCard: React.FC<CharacterCardProps> = ({ character, onPress }) => {
   return (
-    <View>
-      <Text>CharacterCard</Text>
+    <TouchableOpacity
+      onPress={() => onPress(character.id)}
+      activeOpacity={0.7}
+      style={styles.card}
+    >
+    <View style={styles.imageContainer}>
+      <Image source={{ uri: character.image }} style={styles.image} />
     </View>
+
+    </TouchableOpacity>
   )
 }
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 12,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: 'row',
+    overflow: 'hidden',
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+  },
+  imageContainer: {
+    width: 100,
+    height: 140,
+    backgroundColor: '#f0f0f0',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain'
+  }
+})
 
 export default CharacterCard
