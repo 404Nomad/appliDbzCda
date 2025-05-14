aee915e80a08c38746edb8cd506486983d7c11b7
404Nomad <richtofen60@gmail.com>
2025-05-14 10:01:34 +0200

    Refactor Loading component to use TypeScript interface and apply consistent styling

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
-const Loading = ({message = "Chargement..."}) => {
+interface LoadingType {
+    message?: string
+}
+
+const Loading : React.FC<LoadingType> = ({message = "Chargement..."}) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Loading</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    }
+})
 
 export default Loading
