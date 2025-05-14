f0de14866ec3a77fccd070db5ffe2e03175d09b0
404Nomad <richtofen60@gmail.com>
2025-05-14 09:44:26 +0200

    add custom component for the characterDetail and enhance navigation in home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,10 +4,23 @@ import { useDispatch, useSelector } from 'react-redux'
 import { AppDispatch } from '../store/store'
 import { fetchCharacters } from '../store/characters/characterSlice'
 import { RootState } from '../store/store'
+import { useNavigation } from '@react-navigation/native'
 
 const Home = () => {
-  const dispatch = useDispatch<AppDispatch>();
+  const dispatch = useDispatch<AppDispatch>(); // on recupere le hook dispatch
+  const navigate = useNavigation(); // on recupere le hook de navigation
+
   const [currentPage, setCurrentPage] = useState(1); // State to manage the current page, 1 by default
+
+  useEffect(() => {
+    // This effect will run when the component mounts
+  
+    return () => {
+      // This cleanup function will run when the component unmounts
+      // You can perform any necessary cleanup here
+    }
+  }, []); // Empty dependency array means this effect runs once when the component mounts
+  
 
   useEffect(() => {
     dispatch(fetchCharacters(currentPage, 10)); // Fetch characters when the component mounts, page 1 et limit 10
