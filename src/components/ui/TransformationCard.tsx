b04d820729fe620489c64290da78939b1254286c
404Nomad <richtofen60@gmail.com>
2025-05-15 12:03:58 +0200

    Add full-screen image modal to TransformationCard component

--- src/components/ui/TransformationCard.tsx
+++ src/components/ui/TransformationCard.tsx
@@ -1,16 +1,24 @@
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, Image, Dimensions, Modal, SafeAreaView, StatusBar } from 'react-native'
+import React, { useState } from 'react'
 import { Transformation } from '../../types/character'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 interface TransformationCardProps {
     transformation: Transformation
 }
 
 const TransformationCard: React.FC<TransformationCardProps> = ({ transformation }) => {
+
+    const [fullScreenImage, SetFullScreenImage] = useState<string | null>(null);
+    const { width, height } = Dimensions.get('window');
+
     return (
         <View style={styles.container}>
             <TouchableOpacity
-                onPress={() => { console.log('Press') }}
+                onPress={() => {
+                    console.log('Press');
+                    SetFullScreenImage(transformation.image);
+                }}
                 activeOpacity={0.8}
             >
                 <Image
@@ -24,6 +32,34 @@ const TransformationCard: React.FC<TransformationCardProps> = ({ transformation 
                 <Text style={styles.kiLabel}>Ki:</Text>
                 <Text style={styles.kiValue}>{transformation?.ki}</Text>
             </View>
+
+            {/* Modale pour afficher l'image en plein ecran onPress*/}
+            <Modal
+                visible={fullScreenImage !== null}
+                transparent={true}
+                animationType='fade'
+                onRequestClose={() => SetFullScreenImage(null)}
+            >
+                <SafeAreaView style={styles.modalContainer}>
+                    <StatusBar hidden />
+                    <TouchableOpacity
+                        style={styles.closeButton}
+                        onPress={() => SetFullScreenImage(null)}
+                    >
+                        <Icon name='close-circle' size={32} color={'#fff'} />
+                    </TouchableOpacity>
+                    <View style={styles.fullScreenContainer}>
+                        {fullScreenImage && (
+                            <Image
+                                source={{ uri: fullScreenImage }}
+                                style={{ width: width, height: height * 0.8}}
+                                resizeMode='contain'
+                            />
+                        )}
+                    </View>
+                </SafeAreaView>
+
+            </Modal>
         </View>
     )
 }
@@ -69,8 +105,28 @@ const styles = StyleSheet.create({
         fontWeight: '600',
         color: '#FF6b6b',
     },
-
-
+    modalContainer: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.9)',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
+    },
+    closeButton: {
+        position: 'absolute',
+        top: 40,
+        right: 20,
+        zIndex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        borderRadius: 20,
+        padding: 5,
+        elevation: 5,
+    },
+    fullScreenContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 })
 
 export default TransformationCard
