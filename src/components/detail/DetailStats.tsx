b3ebcacd6efa557e983cd9aaf9ed8170b488024d
404Nomad <richtofen60@gmail.com>
2025-05-15 09:54:02 +0200

    Refactor DetailStats component to utilize StatCard for displaying character stats, we modularize ( creating component ) for StatCard

--- src/components/detail/DetailStats.tsx
+++ src/components/detail/DetailStats.tsx
@@ -1,23 +1,22 @@
-import { View, Text, Dimensions, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import React from 'react'
 import { Character } from '../../types/character'
+import StatCard from '../ui/StatCard';
 
 interface DetailStatsProps {
     character: Character | null;
 }
 
 const DetailStats: React.FC<DetailStatsProps> = ({character}) => {
-
-    const {width } = Dimensions.get('window'); // recuperer la largeur de l'ecran
-
-    console.log("width", width); // afficher la largeur de l'ecran dans la console
-
-
   return (
     <View style={styles.statsContainer}>
+        <StatCard label={'Ki'} value={character!.ki} />
+        <StatCard label={'Max Ki'} value={character!.maxKi} />
+        <StatCard label={'Genre'} value={character!.gender} />
+        <StatCard label={'Affiliation'} value={character!.affiliation} />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
     statsContainer: {
@@ -26,7 +25,6 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         marginBottom: 24,
     },
-
-})
+});
 
 export default DetailStats
