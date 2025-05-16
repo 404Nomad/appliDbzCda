b36888fdfac075d73636bee2705463c1045ad57b
404Nomad <richtofen60@gmail.com>
2025-05-16 09:45:30 +0200

    Add ChoiceFighter component and integrate HomeHeader into Fight selection screen

---
+++ src/components/fight/ChoiceFighter.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ChoiceFighter = () => {
+  return (
+    <View>
+      <Text>ChoiceFighter</Text>
+    </View>
+  )
+}
+
+export default ChoiceFighter
--- src/screens/Fight.tsx
+++ src/screens/Fight.tsx
@@ -1,12 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import HomeHeader from '../components/section/HomeHeader'
 
 const Fight = () => {
-  return (
-    <View>
-      <Text>Fight</Text>
-    </View>
-  )
+    return (
+        <View>
+            <HomeHeader />
+            <Text>Fight</Text>
+        </View>
+    )
 }
 
 export default Fight
